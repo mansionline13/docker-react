a64cd59f3e766e268ccfccd39e585a420969fca1
mansi <mansionline13@gmail.com>
2022-05-20 13:21:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React Application</h1>
+            </header>
+            <p className="App-intro">
+                 Hi there!   
+            </p>
+        </div>
+    );
+}
+
+export default App;
